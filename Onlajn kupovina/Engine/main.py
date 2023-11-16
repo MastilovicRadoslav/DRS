60e697139d4d646c554603a4609f9fe18acaf96c
MastilovicRadoslav <radoslav930@gmail.com>
2023-11-16 16:33:58 +0100

    Flask aplikacija

--- Onlajn kupovina/Engine/main.py
+++ Onlajn kupovina/Engine/main.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('pocetna.html')
+
 
 if __name__ == "__main__":
-    app.run(port=8005)
+    app.run(debug=True)
