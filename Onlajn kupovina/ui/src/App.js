c1032e2404987694c4f99b37fcc2f2f57aaa6917
Tojzán Krisztián <kristiantojzan@gmail.com>
2023-11-22 14:09:14 +0100

    Podešavanje ruta

--- Onlajn kupovina/ui/src/App.js
+++ Onlajn kupovina/ui/src/App.js
@@ -4,6 +4,7 @@ import Registracija from './Registration'
 import DodajProizvod from './AddProduct';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Pocetna from './Home';
+import IzmenaProfila from './ModifyProfile';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/Prijava" element={<Prijava />} ></Route>
           <Route path="/Registracija" element={<Registracija />} ></Route>
           <Route path="/Proizvod" element={<DodajProizvod />} ></Route>
+          <Route path="/Profil" element={<IzmenaProfila />} ></Route>
         </Routes>
       </BrowserRouter>
     </div>
