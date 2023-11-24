8dabb8ac0a48f8e95d4d5640573d7d308ffcb997
dovla2001 <vmandic83@gmail.com>
2023-11-24 15:05:24 +0100

    Klasa Proizvod

---
+++ Onlajn kupovina/Engine/Product.py
@@ -0,0 +1,7 @@
+class Proizvod:
+    def __init__(self, naziv, cena, valuta, kolicina, slika):
+        self.naziv = naziv
+        self.cena = cena
+        self.valuta = valuta
+        self.kolicina = kolicina
+        self.slika = slika
