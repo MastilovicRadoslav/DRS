efca77864ab4e367e1555b041b0299881a6906cf
ResProjekat <medic.pr101.2019@uns.ac.rs>
2023-12-12 18:38:52 +0100

    Kartica

--- Onlajn kupovina/Engine/Card.py
+++ Onlajn kupovina/Engine/Card.py
@@ -1,4 +1,15 @@
-class Kartica :
+from config import db
+class Kartica (db.Model):
+    table_name="Kartica",
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    brojKartice = db.Column(db.String, unique=True, nullable=False)
+    datumIsteka = db.Column(db.String, nullable=False)
+    ccv = db.Column(db.String, nullable=False)
+    stanjeNaRacunu = db.Column(db.Float, nullable=False)
+    valuta = db.Column(db.String, nullable=False)
+    odobrena = db.Column(db.String, nullable=False)
+    vlasnik = db.Column(db.String, nullable=False)
+    
     def __init__(self, brojKartice, datumIsteka, ccv, stanjeNaRacunu, valuta, vlasnik, odobrena):
         self.brojKartice = brojKartice
         self.datumIsteka = datumIsteka
@@ -17,8 +28,13 @@ def serializacija_kartice(kartica):
         'ccv': kartica.ccv,
         'stanjeNaRacunu': kartica.stanjeNaRacunu,
         'valuta': kartica.valuta,
-        'vlasnik': {
-            'email': kartica.vlasnik.email
-        },
+        'vlasnik': kartica.vlasnik,
         'odobrena': kartica.odobrena
     }
+    
+#Funkcija za proveru kod uplate da li su valute iste
+def proveraValuta(self, valuta):
+    if self.valuta == valuta:
+        return True
+    else:
+        return False
